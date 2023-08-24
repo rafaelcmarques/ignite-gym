049e25a0d8a52a1c4b2b5eb01b29b09072b49938
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-08-24 20:44:45 -0300

    implementada tela Exercise

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -3,8 +3,8 @@ import { HStack, Heading, Text, VStack } from "native-base";
 export function HistoryCard(){
   return(
     <HStack width={'full'}  bg={"gray.600"} px={"5"} py={"4"} mb={"3"} rounded={"md"} alignItems={"center"} justifyContent={"space-between"}>
-      <VStack>
-        <Heading color={"white"} fontSize={"md"}>
+      <VStack mr={5} flex={1} >
+        <Heading color={"white"} fontSize={"md"} flexShrink={1} textTransform={"capitalize"} fontFamily={"heading"}>
           Costas
         </Heading>
         <Text color={"gray.100"} fontSize={"lg"} numberOfLines={1}>
