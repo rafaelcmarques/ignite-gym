93eb404e0a8f122beef0b6fd16a7334003e5dfda
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-08-25 21:20:10 -0300

    implementado seleção de foto de perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,14 +1,54 @@
 import { useState } from 'react';
-import { Touchable, TouchableOpacity } from 'react-native';
-import {Center, ScrollView, Text, VStack, Skeleton, Heading} from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import {Center, ScrollView, Text, VStack, Skeleton, Heading, useToast} from 'native-base';
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system'
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import { Alert } from 'react-native';
 
 export function Profile(){
   const [photoLoading, setPhotoLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/rafaelcmarques.png')
+  const toast = useToast()
+
+  async function handleUserPhotoSelect() {
+    setPhotoLoading(true)
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4,4],
+        allowsEditing: true,
+      })
+  
+      if(photoSelected.canceled){
+        return;
+      }
+
+      if(photoSelected.assets[0].uri){
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+        if(photoInfo.exists &&  (photoInfo.size / 1024 / 1024 > 5)){
+          return toast.show({
+            title: "Essa imagem é muito grande. Escolha uma de até 5MB",
+            placement:'top',
+            bgColor: 'red.500'
+          })
+         
+        }
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+  
+    } catch (error) {
+        console.log(error)
+    } finally {
+      setPhotoLoading(false)
+    }
+  }
 
   const PHOTO_SIZE = 33
   return (
@@ -28,12 +68,12 @@ export function Profile(){
             /> :
             <UserPhoto 
               size={PHOTO_SIZE} 
-            source={{uri: 'https://github.com/rafaelcmarques.png'}}
+            source={{uri: userPhoto}}
             alt='foto do usário'
             />
         }
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleUserPhotoSelect}>
           <Text color={'green.500'} fontSize={'md'} mt={2} mb={8} fontWeight={'bold'} >
             Alterar foto
           </Text>
